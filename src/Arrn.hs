6e6881cb46d80ce79bdb4e91bdb4d9b3033f2462
Dmitriy Semenov <dmksbk@gmail.com>
2020-04-27 12:35:53 +0300

    Do not use lambda symbol since hLint doesn't work with it

--- src/Arrn.hs
+++ src/Arrn.hs
@@ -19,7 +19,7 @@ newtype Arr3 e1 e2 e3 a = Arr3 {getArr3 :: e1 -> e2 -> e3 -> a}
 
 instance Functor (Arr2 e1 e2) where
   -- fmap f (Arr2 a) = Arr2 $ \ e1 -> (\ e2 -> f $ a e1 e2)
-  fmap f (Arr2 a) = Arr2 $ λ e1 → f . a e1
+  fmap f (Arr2 a) = Arr2 $ \ e1 → f . a e1
 
 instance Functor (Arr3 e1 e2 e3) where
   -- fmap f (Arr3 a) = Arr3 $ \ e1 -> (\ e2 -> (\ e3 -> f $ a e1 e2 e3))
