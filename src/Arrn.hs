333cac5ce998c1b61d7d9ce7640e6fb91c7d5ccb
Dmitriy Semenov <dmksbk@gmail.com>
2020-04-29 00:31:45 +0300

    Prog 1.2.3 - Applicative for Arr2 and Arr3

--- src/Arrn.hs
+++ src/Arrn.hs
@@ -11,8 +11,22 @@ newtype Arr3 e1 e2 e3 a = Arr3 {getArr3 :: e1 -> e2 -> e3 -> a}
 
 instance Functor (Arr2 e1 e2) where
   -- fmap f (Arr2 a) = Arr2 $ \ e1 -> (\ e2 -> f $ a e1 e2)
-  fmap f (Arr2 a) = Arr2 $ \ e1 → f . a e1
+  -- fmap f (Arr2 a) = Arr2 $ \ e1 → f . a e1
+  fmap f = Arr2 . (fmap . fmap $ f) . getArr2
 
 instance Functor (Arr3 e1 e2 e3) where
   -- fmap f (Arr3 a) = Arr3 $ \ e1 -> (\ e2 -> (\ e3 -> f $ a e1 e2 e3))
-  fmap f (Arr3 a) = Arr3 $ \ e1 e2 → f . a e1 e2
+  -- fmap f (Arr3 a) = Arr3 $ \ e1 e2 → f . a e1 e2
+  fmap f = Arr3 . (fmap . fmap . fmap $ f) . getArr3
+  
+-- Prog 1.2.3  
+-- Сделайте типы данных Arr2 e1 e2 и Arr3 e1 e2 e3 представителями класса типов Applicative с естественной семантикой двух и трех окружений:
+
+instance Applicative (Arr2 e1 e2) where
+  -- pure a = Arr2 $ \ e1 e2 -> a
+  pure = Arr2 . const . const
+  (Arr2 f) <*> (Arr2 v) = Arr2 $ \ e1 e2 -> f e1 e2 (v e1 e2) 
+
+instance Applicative (Arr3 e1 e2 e3) where
+  pure = Arr3 . const . const . const
+  (Arr3 f) <*> (Arr3 v) = Arr3 $ \ e1 e2 e3 -> f e1 e2 e3 (v e1 e2 e3) 
--- test/ArrnSpec.hs
+++ test/ArrnSpec.hs
@@ -6,13 +6,21 @@ import Arrn
 import Test.Hspec
 
 spec :: Spec
-spec = 
+spec = do
   describe "Prog 1.1.1 - instance Functor Arr2 and Arr3 " $
-  do
-    it "Arr2 is Functor" $
-      getArr2 (fmap length (Arr2 take)) 10 "abc"
-        `shouldBe` 3
+    do
+      it "Arr2 is Functor" $
+        getArr2 (fmap length (Arr2 take)) 10 "abc"
+          `shouldBe` 3
+      it "Arr3 is Functor" $
+        getArr3 (tail <$> tail <$> Arr3 zipWith) (+) [1,2,3,4] [10,20,30,40,50]
+          `shouldBe` [33,44]
     
-    it "Arr3 is Functor" $
-      getArr3 (tail <$> tail <$> Arr3 zipWith) (+) [1,2,3,4] [10,20,30,40,50]
-        `shouldBe` [33,44]
+  describe "Prog 1.2.3 - instance Applicative Arr2 and Arr3" $
+    do
+      it "Arr2 is Applicative" $
+        getArr2 (Arr2 (\x y z -> x+y-z) <*> Arr2 (*)) 2 3
+          `shouldBe` -1
+      it "Arr3 is Applicative" $
+        getArr3 (Arr3 (\x y z w -> x+y+z-w) <*> Arr3 (\x y z -> x*y*z)) 2 3 4
+          `shouldBe` -15
