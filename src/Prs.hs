f3b77441db38b101030c305c7078ca82c5bd45a0
Dmitriy Semenov <dmksbk@gmail.com>
2020-05-01 16:39:57 +0300

    Prog 1.4.2 - Applicative Prs

--- src/Prs.hs
+++ src/Prs.hs
@@ -3,6 +3,7 @@ module Prs where
 -- Предположим, тип парсера определен следующим образом:
 newtype Prs a = Prs { runPrs :: String -> Maybe (a, String) }
 
+-- Prg 1.4.1
 -- Сделайте этот парсер представителем класса типов Functor.
 instance Functor Prs where
   fmap f p = Prs go where
@@ -18,3 +19,27 @@ anyChr = Prs go where
   go []     = Nothing
   go (x:xs) = Just (x, xs)
 
+-- Prg 1.4.2
+-- Сделайте парсер Prs a из предыдущей задачи аппликативным функтором с естественной для парсера семантикой:
+
+instance Applicative Prs where
+  pure a = Prs go where
+    go s = Just (a, s)
+  fa <*> va = Prs go where
+    go s = case runPrs fa s of
+      Nothing -> Nothing
+      Just (f, s') -> case runPrs va s' of
+        Nothing -> Nothing
+        Just (v, s'') -> Just (f v, s'')
+
+-- Another solution using monad comprehension
+--{-# LANGUAGE MonadComprehensions #-}
+--
+--instance Applicative Prs where
+--  pure v  = Prs fun where
+--    fun xs = Just (v, xs)
+--  fp <*> vp = Prs fun where
+--    fun s = [ (f v, s'')
+--            | (f, s')    <- runPrs fp s
+--            , (v, s'')   <- runPrs vp s'
+--            ]
--- test/PrsSpec.hs
+++ test/PrsSpec.hs
@@ -5,7 +5,7 @@ import Prs
 import Data.Char (digitToInt)
 
 spec :: Spec
-spec =
+spec = do
   describe "Prog 1.4.1 - Functor Prs" $
     do
       it "Prs is Functor (Just Char)" $
@@ -15,3 +15,11 @@ spec =
       it "Prs is Functor (Just Int)" $
         runPrs (digitToInt <$> anyChr) "BCD" `shouldBe` Just (11,"CD")
 
+  describe "Prog 1.4.2 - Applicative Prs" $
+    do
+      it "Parse 3 chars" $
+        runPrs ((,,) <$> anyChr <*> anyChr <*> anyChr) "ABCDE"
+          `shouldBe` Just (('A','B','C'),"DE")
+      it "Parse 2 chars, ignoring first one" $
+        runPrs (anyChr *> anyChr) "ABCDE"
+          `shouldBe` Just ('B',"CDE")
