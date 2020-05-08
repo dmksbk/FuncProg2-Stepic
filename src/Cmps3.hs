76f8f964590e5d00739dff5e79a6db1064b940e3
Dmitriy Semenov <dmksbk@gmail.com>
2020-05-08 21:48:47 +0300

    Pointless notation for Functor Cmps 3

--- src/Cmps3.hs
+++ src/Cmps3.hs
@@ -6,4 +6,5 @@ newtype Cmps3 f g h a = Cmps3 { getCmps3 :: f (g (h a)) }
 -- представителем класса типов Functor при условии, что первые его три параметра являются функторами:
 
 instance (Functor f1, Functor f2, Functor f3) => Functor (Cmps3 f1 f2 f3) where
-  fmap h (Cmps3 x) = Cmps3 $ fmap (fmap (fmap h)) x
+  -- fmap h (Cmps3 x) = Cmps3 $ fmap (fmap (fmap h)) x
+  fmap h = Cmps3 . fmap (fmap (fmap h)) . getCmps3
