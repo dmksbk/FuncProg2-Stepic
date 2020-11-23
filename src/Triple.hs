0e3ff6513cb179fc3951db578f6a02a72fb2a71c
Dmitriy Semenov <dmksbk@gmail.com>
2020-11-23 23:46:23 +0300

    Prog 2.1.3 - Сделайте тип Triple представителем класса типов Foldable

--- src/Triple.hs
+++ src/Triple.hs
@@ -13,4 +13,9 @@ instance Applicative Triple where
     pure a = Tr a a a
     (Tr fa fb fc) <*> (Tr a b c) = Tr (fa a) (fb b) (fc c)
 
+-- Prog 2.1.3
+-- Сделайте тип Triple представителем класса типов Foldable:
 
+instance Foldable Triple where
+    foldr f ini (Tr a b c) = a `f` (b `f` (c `f` ini))
+    foldl f ini (Tr a b c) = ((ini `f` a) `f` b) `f` c
--- test/TripleSpec.hs
+++ test/TripleSpec.hs
@@ -1,15 +1,22 @@
 module TripleSpec where
 
 import Triple
-import Test.Hspec
+import Test.Hspec ( describe, it, shouldBe, Spec )
 
 spec :: Spec
-spec = 
+spec = do
   describe "Prog 1.1.2 - instance Functor Triple and instance Applicative Functor" $
-  do 
-    it "Triple is Functor" $
-      (^2) <$> Tr 1 (-2) 3 `shouldBe` Tr 1 4 9
-      
-    it "Triple is Applicative" $
-      Tr (^2) (+2) (*3) <*> Tr 2 3 4 `shouldBe` Tr 4 5 12
+    do 
+      it "Triple is Functor" $
+        (^2) <$> Tr 1 (-2) 3 `shouldBe` Tr 1 4 9
+        
+      it "Triple is Applicative" $
+        Tr (^2) (+2) (*3) <*> Tr 2 3 4 `shouldBe` Tr 4 5 12
+    
+  describe "Prog 2.1.3 - Сделайте тип Triple представителем класса типов Foldable" $
+    do
+      it "abcdefg!!" $
+        foldr (++) "!!" (Tr "ab" "cd" "efg") `shouldBe` "abcdefg!!"
+      it "!!abcdefg" $
+        foldl (++) "!!" (Tr "ab" "cd" "efg") `shouldBe` "!!abcdefg"
 
